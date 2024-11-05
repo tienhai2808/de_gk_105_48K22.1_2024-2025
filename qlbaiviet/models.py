30d64603470f571dc0996a3f89021493b1aa413f
Hải Tân <221124022310@due.udn.vn>
2024-11-05 17:09:38 +0700

    Commit 2

--- qlbaiviet/models.py
+++ qlbaiviet/models.py
@@ -5,17 +5,22 @@ from django.contrib.auth.models import User
 class ChuyenMuc(models.Model):
   ten = models.CharField(max_length=200)
   
+  def __str__(self):
+    return self.ten
+  
   
 class BaiViet(models.Model):
   tieu_de = models.CharField(max_length=250)
-  noi_dung = models.TextField(blank=True, null=True)
+  noi_dung = models.TextField(blank=True)
   ngay_tao = models.DateField()
   nguoi_tao = models.ForeignKey(User, on_delete=models.CASCADE)
   chuyen_muc = models.ManyToManyField(ChuyenMuc, through='BaiVietChuyenMuc')
+  
+  def __str__(self):
+    return self.tieu_de
   
 
 class BaiVietChuyenMuc(models.Model):
   bai_viet = models.ForeignKey(BaiViet, on_delete=models.CASCADE)
   chuyen_muc = models.ForeignKey(ChuyenMuc, on_delete=models.CASCADE)
   thu_tu = models.IntegerField()
-  
--- qlbaiviet/views.py
+++ qlbaiviet/views.py
@@ -3,6 +3,8 @@ from .models import *
 from .forms import BaiVietForm
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.db.models import Max
+import numpy as np
 
 # Create your views here.
 def index(request):
@@ -10,7 +12,8 @@ def index(request):
 
 def xembaiviet(request, idbaiviet):
   bai_viet = get_object_or_404(BaiViet, id=idbaiviet)
-  return render(request, 'xembaiviet.html', {'baiviet': bai_viet})
+  tieu_de = bai_viet.tieu_de
+  return render(request, 'xembaiviet.html', {'baiviet': bai_viet, 'title': tieu_de})
 
 def dsbaiviet(request, idchuyenmuc):
   chuyen_muc = get_object_or_404(ChuyenMuc, id=idchuyenmuc)
@@ -25,19 +28,24 @@ def suabaiviet(request, idbaiviet=None):
     button = 'Sửa'
   else:
     bai_viet = None
-    title = 'TẠO BÀI VIẾT'
+    title ='TẠO BÀI VIẾT'
     button = 'Tạo'
   form = BaiVietForm(instance=bai_viet)
   if request.POST:
     form = BaiVietForm(request.POST, instance=bai_viet)
     if form.is_valid():
       bai_viet_update = form.save(False)
-      if not bai_viet:
-        bai_viet_update.nguoi_tao = User.objects.get(username='caotienhai')
+      if not idbaiviet:
+        rd_id = np.random.randint(1,5)
+        bai_viet_update.nguoi_tao = User.objects.get(id=rd_id)
       bai_viet_update.save()
-      if bai_viet:
+      for chuyen_muc in form.cleaned_data['chuyen_muc']:
+        tt_max = BaiVietChuyenMuc.objects.filter(chuyen_muc=chuyen_muc).aggregate(Max('thu_tu'))['thu_tu__max'] or 0
+        BaiVietChuyenMuc.objects.create(bai_viet=bai_viet_update, chuyen_muc=chuyen_muc, thu_tu=tt_max+1)
+      # form.save_m2m()
+      if idbaiviet:
         messages.success(request, f'Chỉnh sửa bài viết {bai_viet_update.tieu_de} thành công')
       else:
         messages.success(request, f'Tạo mới bài viết {bai_viet_update.tieu_de} thành công')
       return redirect('xembaiviet', bai_viet_update.id)
-  return render(request, 'suabaiviet.html', {'title': title, 'form': form, 'baiviet': bai_viet, 'button': button})
+  return render(request, 'suabaiviet.html', {'form': form, 'title': title, 'button': button})
